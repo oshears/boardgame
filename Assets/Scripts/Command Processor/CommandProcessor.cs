732c98a3a51109ce466169dadfe6d0f2e19f6016
Osaze Shears <yoze15@gmail.com>
2024-09-17 16:09:58 -0400

    Added readme

--- Assets/Scripts/Command Processor/CommandProcessor.cs
+++ Assets/Scripts/Command Processor/CommandProcessor.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-namespace{
+namespace {
 
     public class CommandProcessor : MonoBehaviour {
     
---
+++ README.md
@@ -0,0 +1,2 @@
+# Board Game
+A package for making turn-based board games in Unity.
