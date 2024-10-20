098f36ecb064137406d03cf5f3ffbedfe14e52a1
Osaze Shears <yoze15@gmail.com>
2024-10-19 22:01:20 -0400

    Added item deck SO

---
+++ Assets/Scripts/Items/ItemDeck.cs
@@ -0,0 +1,9 @@
+using OSGames.BoardGame.Generic;
+using UnityEngine;
+
+namespace OSGames.BoardGame {
+    [CreateAssetMenu(fileName = "ItemDeck", menuName = "Board Game/Deck/Item Deck", order = 0)]
+    public class ItemDeck : Deck<Item> {
+
+    }
+}
